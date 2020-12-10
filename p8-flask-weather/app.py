53bd549fbc80219b5d5f86b846b66cff48b7383a
Niko Vrdoljak <niko@mailtastic.com>
2020-12-10 21:56:55 +0100

    Dodan prvi dio P8 weather aplikacije

--- p8-flask-weather/app.py
+++ p8-flask-weather/app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+import os
 import requests
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 
 app.config['BOOTSTRAP_BOOTSWATCH_THEME'] = 'minty'
-
+OPEN_WEATHER_API_KEY = os.environ.get('OPEN_WEATHER_API_KEY')
 
 @app.route('/')
 def index():
     url = 'https://api.openweathermap.org/data/2.5/weather'
-    parameters = {'q': 'zadar', 'appid': 'e0fc4312cb583fa27eb794af4c75f574', 'units': 'metric', 'lang': 'hr'}
+    parameters = {'q': 'zadar', 'appid': OPEN_WEATHER_API_KEY, 'units': 'metric', 'lang': 'hr'}
     response = requests.get(url, parameters)
     weather = response.json()
     return render_template('index.html', weather = weather)
